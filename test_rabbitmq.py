11d12d089cb9aba759a320cc5bbf1ca80a8b4d5a
pengchao2022 <pengchao.ma2@outlook.com>
2026-08-20 16:46:51 +0800

    feat: GTP-54273 added the code

--- test_rabbitmq.py
+++ test_rabbitmq.py
@@ -42,49 +42,49 @@ def test_rabbitmq_connection(creds):
     )
 
     try:
-        # 1. 建立连接
+        # create connection
         connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
-        print("✅ 成功连接到 RabbitMQ!")
+        print("✅ Successfully connect RabbitMQ!")
 
-        # 2. 声明一个测试队列 (Queue)
+        # declare a test queue
         queue_name = 'test_aws_queue'
         channel.queue_declare(queue=queue_name, durable=False)
 
-        # 3. 发送一条测试消息
+        # send a test message
         message = "Hello from EC2 via Secrets Manager & RabbitMQ!"
         channel.basic_publish(
             exchange='',
             routing_key=queue_name,
             body=message
         )
-        print(f"📤 已成功发送测试消息: '{message}'")
+        print(f"📤 Successfully sent the message: '{message}'")
 
-        # 4. 消费这条测试消息进行验证
+        # consume the test message
         # method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
         # if method_frame:
-        #     print(f"📥 成功接收到测试消息: '{body.decode()}'")
+        #     print(f"📥 Successfully received the message: '{body.decode()}'")
         # else:
-        #     print("⚠️ 未能接收到刚才发送的消息")
+        #     print("⚠️ did not get the message")
 
-        # # 5. 关闭连接
+        # # close the connection
         # connection.close()
-        # print("🎉 测试全部通过！")
+        # print("All Passed！")
 
     except Exception as e:
-        print(f"❌ RabbitMQ 连接或操作失败: {e}")
+        print(f"❌ RabbitMQ failed to connect: {e}")
 
 if __name__ == "__main__":
-    # 替换为你实际在 AWS Secrets Manager 中创建的 Secret 名称或 ARN
+    
     SECRET_NAME = "secret-for-rabbitmq-int"
-    REGION = "us-east-1"  # 换成你的 AWS 区域
+    REGION = "us-east-1"  
 
     try:
-        # 第一步：拉取凭据
+        # get the credentials
         secret_data = get_secret_from_aws(SECRET_NAME, REGION)
         
-        # 第二步：测试 RabbitMQ
+        # test RabbitMQ
         test_rabbitmq_connection(secret_data)
         
     except Exception as e:
-        print(f"💥 脚本运行出错: {e}")
+        print(f"error : {e}")
